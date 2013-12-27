2a354dca212ba5d0fbfd9f553f8f367f3bd81661
Niklas Hambüchen <mail@nh2.me>
2013-12-27 08:59:22 +0000

    Some Haddocks

--- src/Honi.hs
+++ src/Honi.hs
@@ -1,8 +1,23 @@
 {-# LANGUAGE ForeignFunctionInterface #-}
 
+-- | Binding for the OpenNI C API (OniCAPI.h).
+--
+-- Example use:
+--
+-- >import Honi
+-- >import Honi.Types
+-- >
+-- >initialize 2
+-- >Right (di:_) <- getDeviceList
+-- >Right d      <- deviceOpenInfo di
+-- >deviceGetSensorInfo d SensorDepth
+--
+-- This module does not use `error`.
+-- All its potential errors can be caught as `HoniBug`.
 module Honi
-  (ApiVersion
+  ( ApiVersion
   , Oni
+  -- * General
   , initialize, shutdown
   , getDeviceList
   , deviceOpen
@@ -11,6 +26,7 @@ module Honi
   , deviceGetSensorInfo
   , deviceCreateStream
 
+  -- * Streams
   , streamStart
   , streamStop
   )
@@ -23,18 +39,25 @@ import Foreign.C
 
 import Honi.Types
 
+-- Internal use only: for when we don't want to convert to a `Status`.
 type OniStatus = CInt
+
+-- | An OpenNI action that can fail with a `Status`.
 type Oni a = IO (Either Status a)
 
 foreign import ccall unsafe "OniCAPI.h oniInitialize"
   oniInitialize :: CInt -> IO OniStatus
 
+-- | Numerical version of the OpenNI API.
 type ApiVersion = Int
 
+
+-- | Initialize OpenNI2. Must be called before any other Honi IO functions.
 initialize :: ApiVersion -> IO Status
 initialize version
   = fromCInt <$> oniInitialize (fromIntegral version)
 
+-- | Shutdown OpenNI2.
 foreign import ccall unsafe "OniCAPI.h oniShutdown"
   shutdown :: IO ()
 
@@ -44,6 +67,8 @@ foreign import ccall unsafe "OniCAPI.h oniGetDeviceList"
 foreign import ccall unsafe "OniCAPI.h oniReleaseDeviceList"
   oniReleaseDeviceList :: Ptr DeviceInfo -> IO OniStatus
 
+-- | Get the list of currently connected device.
+-- Each device is represented by its `DeviceInfo`.
 getDeviceList :: Oni [ DeviceInfo ]
 getDeviceList = alloca $ \listPtr -> alloca $ \numPtr ->
   whenOK (oniGetDeviceList listPtr numPtr) $ do
@@ -53,6 +78,7 @@ getDeviceList = alloca $ \listPtr -> alloca $ \numPtr ->
     oniReleaseDeviceList list
     return $ Right deviceList
 
+-- | Run the given action if `StatusOK` was returned from the first one.
 whenOK :: IO OniStatus -> Oni a -> Oni a
 whenOK oni ok = do
   r <- oni
@@ -63,6 +89,7 @@ whenOK oni ok = do
 foreign import ccall unsafe "OniCAPI.h oniDeviceOpen"
   oniDeviceOpen :: CString -> Ptr OpaquePtr -> IO OniStatus
 
+-- | Open a device from a device URI.
 deviceOpen :: BS.ByteString -> Oni DeviceHandle
 deviceOpen devUri
   = BS.useAsCString devUri $ \uriPtr ->
@@ -70,18 +97,22 @@ deviceOpen devUri
         whenOK (oniDeviceOpen uriPtr handlePtr)
           ((Right . DeviceHandle) <$> peek handlePtr)
 
+-- | Open a device from a `DeviceInfo`.
 deviceOpenInfo :: DeviceInfo -> Oni DeviceHandle
 deviceOpenInfo = deviceOpen . uri
 
 foreign import ccall unsafe "OniCAPI.h oniDeviceClose"
   oniDeviceClose :: OpaquePtr -> IO OniStatus
 
+-- | Close a device.
 deviceClose :: DeviceHandle -> IO Status
 deviceClose (DeviceHandle p) = fromCInt <$> oniDeviceClose p
 
 foreign import ccall unsafe "OniCAPI.h oniDeviceGetSensorInfo"
   oniDeviceGetSensorInfo :: OpaquePtr -> CInt -> IO (Ptr SensorInfo)
 
+-- | Get the possible configurations available for a specific source,
+-- or @Nothing@ if the source does not exist.
 deviceGetSensorInfo :: DeviceHandle -> SensorType -> IO (Maybe SensorInfo)
 deviceGetSensorInfo (DeviceHandle p) sType = do
   sip <- oniDeviceGetSensorInfo p (toCInt sType)
@@ -94,6 +125,7 @@ deviceGetSensorInfo (DeviceHandle p) sType = do
 foreign import ccall unsafe "OniCAPI.h oniDeviceCreateStream"
   oniDeviceCreateStream :: OpaquePtr -> CInt -> Ptr OpaquePtr -> IO OniStatus
 
+-- | Create a new stream in the device. The stream will originate from the source.
 deviceCreateStream :: DeviceHandle -> SensorType -> Oni StreamHandle
 deviceCreateStream (DeviceHandle dh) st = alloca $ \streamPtr ->
   whenOK (oniDeviceCreateStream dh (toCInt st) streamPtr)
@@ -102,11 +134,13 @@ deviceCreateStream (DeviceHandle dh) st = alloca $ \streamPtr ->
 foreign import ccall unsafe "OniCAPI.h oniStreamStart"
   oniStreamStart :: OpaquePtr -> IO OniStatus
 
+-- | Start generating data from the stream.
 streamStart :: StreamHandle -> IO Status
 streamStart (StreamHandle sh) = fromCInt <$> oniStreamStart sh
 
 foreign import ccall unsafe "OniCAPI.h oniStreamStop"
   oniStreamStop :: OpaquePtr -> IO ()
 
+-- | Stop generating data from the stream.
 streamStop :: StreamHandle -> IO ()
 streamStop (StreamHandle sh) = oniStreamStop sh
